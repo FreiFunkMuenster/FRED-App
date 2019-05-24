80db0eca2eb862dc8476e125215954118d7adf7d
Jonas Looser <1stKarkan@gmail.com>
2019-05-24 09:22:45 +0200

    readded persistent logging

--- app/src/main/java/de/florian_adelt/fred/helper/Logger.java
+++ app/src/main/java/de/florian_adelt/fred/helper/Logger.java
@@ -15,7 +15,7 @@ public class Logger {
     public static void log(Context context, String tag, String message, int level) {
 
         DatabaseHelper helper = new DatabaseHelper(context);
-        //helper.log(message, level, tag);  // todo: crashes on Android 9 device with: table Logs has no column named tag
+        helper.log(message, level, tag);  // todo: crashes on Android 9 device with: table Logs has no column named tag
         Log.e("fred_" + tag, message);
 
     }
